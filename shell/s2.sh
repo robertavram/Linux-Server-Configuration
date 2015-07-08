f38ec25744fe445003c1a8c8ba08609d8008766e
Robert Avram <robert@ravdev.com>
2015-07-08 14:39:03 -0700

    mac2

--- shell/s2.sh
+++ shell/s2.sh
@@ -27,14 +27,17 @@ apt-get install -y fail2ban
 # make a conf backup
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jailconf.local
 
+# set bantime to 1800 sec
 word="bantime[[:space:]]*=[[:space:]][[:digit:]]*"
 rep="bantime = 1800"
 sed -i "s/${word}/${rep}/" /etc/fail2ban/jail.local
 
+# set the email to my email
 word="destemail[[:space:]]*=[[:space:]]root@localhost"
 rep="destemail = robert@ravdev.com"
 sed -i "s/${word}/${rep}/" /etc/fail2ban/jail.local
 
+# set the sender to some recongnizable name
 word="sendername[[:space:]]*=[[:space:]]Fail2Ban"
 rep="sendername = FlutterHub_Server_Fail2Ban"
 sed -i "s/${word}/${rep}/" /etc/fail2ban/jail.local
@@ -55,8 +58,6 @@ apt-get install -y sendmail
 # system monitoring tools
 apt-get install -y python-pip build-essential python-dev
 pip install Glances
-
-
 
 # install docker
 # make sure wget is installed
@@ -115,8 +116,6 @@ docker run --restart=always -d -v /var/log/apache2:/var/log/apache2 -p 80:80 --n
 
 # auto system check for app if it's still working
 # 
-
-
 
 
 
